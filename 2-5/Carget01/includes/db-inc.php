62452cbf5f9657f18fdb2cd53cd52fa718b255db
Luger <sabertxt@gmail.com>
2018-05-03 03:41:39 +0800

    Revert "Formatted directory structures"
    This reverts commit 354074295b1dbb57dbdb8f617f53e85e4c844257.

--- 2-5/Carget01/includes/db-inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-  $dbServerName = "localhost";
-  $dbUserName = "root";
-  $dbPassword = "";
-  $dbName = "carget_db";
-
-  $mysqli = new mysqli($dbServerName,$dbUserName ,$dbPassword ,$dbName );
